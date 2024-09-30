e86d797cf0df2ccad85da7f73234c2699e60c967
Abby Crockett <arcrocs21@gmail.com>
2024-09-30 14:51:51 -0500

    Fixed minor table format error

--- p2-frontend/src/views/CourseList.vue
+++ p2-frontend/src/views/CourseList.vue
@@ -6,30 +6,34 @@
         <br>
         <div class="table-container">
             <table class="course-table">
-                <tr>
-                    <th>Name</th>
-                    <th>Department</th>
-                    <th>Number</th>
-                    <th>Level</th>
-                    <th>Hours</th>
-                    <th>Actions</th>
-                </tr>
-                <tr>
-                    <td>Programming I</td>
-                    <td>CMSC</td>
-                    <td>1234</td>
-                    <td>01</td>
-                    <td>3</td>
-                    <td>
-                        <div class="button-container">
-                            <button type="button" class="update-button">UPDATE</button>
-                            <button type="button" class="delete-button">DELETE</button>
-                            <router-link to="/view-course">
-                                <button type="button" class="view-button">VIEW</button>
-                            </router-link>
-                        </div>
-                    </td>
-                </tr>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Department</th>
+                        <th>Number</th>
+                        <th>Level</th>
+                        <th>Hours</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Programming I</td>
+                        <td>CMSC</td>
+                        <td>1234</td>
+                        <td>01</td>
+                        <td>3</td>
+                        <td>
+                            <div class="button-container">
+                                <button type="button" class="update-button">UPDATE</button>
+                                <button type="button" class="delete-button">DELETE</button>
+                                <router-link to="/view-course">
+                                    <button type="button" class="view-button">VIEW</button>
+                                </router-link>
+                            </div>
+                        </td>
+                    </tr>
+                </tbody>
             </table>
         </div>
     </div>
@@ -47,5 +51,12 @@ export default {
 
 <style scoped>
 @import '@/assets/styles.css';
+
+.table-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+}
 </style>
   
